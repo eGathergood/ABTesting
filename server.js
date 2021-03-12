8fab4a4434eb1c3a5891d2ed609335e4bfc6546f
eGathergood <ewan_good@hotmail.co.uk>
2021-03-12 13:48:27 +0000

    change get path

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/', (req, res) => {
 })
 
 // app.use(express.static(path.join(__dirname, 'client', 'build')))
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(
         path.join(__dirname, 'client/public/index.html'),
         function (err) {
