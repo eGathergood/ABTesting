57d34367b25a820f9d4cf58d950e369f580d790c
eGathergood <ewan_good@hotmail.co.uk>
2021-03-12 11:51:44 +0000

    sad

--- server.js
+++ server.js
@@ -130,8 +130,8 @@ app.get('/', (req, res) => {
 })
 
 // app.use(express.static(path.join(__dirname, 'client', 'build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build'))
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/public/index.html'))
 })
 
 app.listen(PORT, () => {
