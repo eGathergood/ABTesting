7df3afee78dd9c7ec1ed3f7bc7f167651aa44ff0
eGathergood <ewan_good@hotmail.co.uk>
2021-03-12 11:45:14 +0000

    ahhhh

--- server.js
+++ server.js
@@ -130,9 +130,9 @@ app.get('/', (req, res) => {
 })
 
 // app.use(express.static(path.join(__dirname, 'client', 'build')))
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build'))
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build'))
+})
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
